35ef07a27840abe96a59bffe595ba14329b18541
Milos Mosovsky <milos.mosovsky@gmail.com>
2015-06-26 01:07:23 +0200

    Add model

--- models/User.scala
+++ models/User.scala
@@ -5,7 +5,7 @@ import lib.{MongoModel, MongoTable}
 import play.api.libs.json._
 case class User( browser: String )
 
-object User extends MongoTable[User]("visits") with MongoModel {
+object User extends MongoTable[User]("user") with MongoModel {
   implicit val userFormat = Json.format[User]
   def validator(json: JsValue) = json.validate[User]
 }
